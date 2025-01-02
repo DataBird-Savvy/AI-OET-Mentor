dd833523b7e48f32c898c37fcc4a79df18cbb49e
Jiya Mary Joseph <mejiyamaryjoseph@gmail.com>
2025-01-02 13:17:25 +0530

    edited

--- app.py
+++ app.py
@@ -432,5 +432,5 @@ def reading_task():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
